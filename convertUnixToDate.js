bedcf55934e25cb793bf281655af812be4354501
ImamBaihaqi <imam.baihaqi2396@gmail.com>
2024-05-22 10:37:47 +0700

    Submit task first session

--- convertUnixToDate.js
+++ convertUnixToDate.js
@@ -3,9 +3,13 @@ Gunakan npm dayjs (https://www.npmjs.com/package/dayjs) untuk menyelesaikan tuga
 Convert Unix timestamp menjadi tanggal dengan format DD/MM/YYYY
 */
 
+const dayjs = require('dayjs');
+
 function convertUnixToDate(unixTimestamp) {
   // Code here
+  const date = dayjs.unix(unixTimestamp).format('DD/MM/YYYY');
+  console.log(date);
 }
 
-convertUnixToDate(1710867600) // Output: 20/03/2024
-convertUnixToDate(1714150800) // Output: 27/04/2024
+convertUnixToDate(1710867600); // Output: 20/03/2024
+convertUnixToDate(1714150800); // Output: 27/04/2024
--- numberGrouping.js
+++ numberGrouping.js
@@ -7,6 +7,23 @@ Kelompokkan angka dalam 3 nested array.
 
 function numberGrouping(numbers) {
   // Code here
+  const genap = [];
+  const ganjil = [];
+  const bagi3 = [];
+
+  numbers.forEach((num) => {
+    if (num % 2 === 0) {
+      genap.push(num);
+    } else {
+      ganjil.push(num);
+    }
+
+    if (num % 3 === 0) {
+      bagi3.push(num);
+    }
+  });
+
+  console.log([genap, ganjil, bagi3]);
 }
 
-numberGrouping([1, 2, 4, 6, 8]) // Output: [[2, 4, 8], [1], [6]]
+numberGrouping([1, 2, 4, 6, 8]); // Output: [[2, 4, 8], [1], [6]]
